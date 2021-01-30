05c47657e3bbd66f2364f5dd4244c9c60d286372
Maksym <maksymRozhko7@gmail.com>
2021-01-30 16:22:36 +0200

    feat(electron): live-reload

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const { app, BrowserWindow } = require('electron');
 app.whenReady()
     .then(() => {
         const win = new BrowserWindow({
-            alwaysOnTop: true,
+            // alwaysOnTop: true,
             thickFrame: true,
             frame: false,
             // fullscreen: true,
         });
-        win.loadFile('dist/index.html');
+        win.loadURL('http://localhost:1234');
+        // win.loadFile('dist/index.html');
         // win.webContents.openDevTools();
     });
